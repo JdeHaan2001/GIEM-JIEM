bf7b50308a3243301ef619269f6e8445fd20d708
Jeroen de Haan <info.jdehaan@gmail.com>
2022-02-27 15:43:55 +0100

    Ikd what changed

--- Assets/Scripts/Music/SoundEmitterContinuous.cs
+++ Assets/Scripts/Music/SoundEmitterContinuous.cs
@@ -7,7 +7,6 @@ public class SoundEmitterContinuous : SoundEmitter
     // Start is called before the first frame update
     void Start()
     {
-        audiosou
     }
 
     // Update is called once per frame
